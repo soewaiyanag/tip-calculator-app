14ac6aea54368b480a1fd110eb29afdc210b4fbb
soewaiyanag <soewaiyanag@gmail.com>
2022-01-06 19:14:22 +0630

    change class to functional component

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,32 +1,30 @@
 import React from "react";
 import "../styles/Overview.scss";
 
-class Overview extends React.Component {
-  render() {
-    const tip = Number(this.props.tipAmount());
-    const total = Number(this.props.total());
-    return (
-      <section className="Overview">
-        <div>
-          <div className="row">
-            <h3 className="label">Tip Amount</h3>
-            <h1 className="value">${tip}</h1>
-          </div>
-          <div className="row">
-            <h3 className="label">Total</h3>
-            <h1 className="value">${total}</h1>
-          </div>
+const Overview = (props) => {
+  const tip = Number(props.tipAmount());
+  const total = Number(props.total());
+  return (
+    <section className="Overview">
+      <div>
+        <div className="row">
+          <h3 className="label">Tip Amount</h3>
+          <h1 className="value">${tip}</h1>
         </div>
-        <button
-          className={tip + total ? null : "disable"}
-          id="reset"
-          onClick={tip + total ? this.props.reset : null}
-        >
-          RESET
-        </button>
-      </section>
-    );
-  }
-}
+        <div className="row">
+          <h3 className="label">Total</h3>
+          <h1 className="value">${total}</h1>
+        </div>
+      </div>
+      <button
+        className={tip + total ? null : "disable"}
+        id="reset"
+        onClick={tip + total ? props.reset : null}
+      >
+        RESET
+      </button>
+    </section>
+  );
+};
 
 export default Overview;
